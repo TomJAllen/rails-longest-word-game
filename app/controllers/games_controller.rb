404825edd93e3309544b16895260c141ad4eefce
Tom Allen <tomallen_1@hotmail.co.uk>
2023-01-08 14:39:09 +1100

    update

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -11,14 +11,14 @@ class GamesController < ApplicationController
     @letters = params[:letters].chars
     @guess_array = @guess.chars
     @url = "https://wagon-dictionary.herokuapp.com/#{@guess}"
-    word_details = URI.open(@url).read
-    word = JSON.parse(word_details)
+    @word_details = URI.open(@url).read
+    @word = JSON.parse(@word_details)
 
     if (@guess_array - @letters).any?
       @message = "Sorry but #{@guess.upcase} can't be built out of #{@letters}!"
-    elsif (@guess_array - @letters).empty? && word["found"] == false
+    elsif (@guess_array - @letters).empty? && @word["found"] == false
       @message = "Sorry but #{@guess.upcase} doesn't seem to be a valid english word"
-    elsif (@guess_array - @letters).empty? && word["found"] == true
+    elsif (@guess_array - @letters).empty? && @word["found"] == true
       @message = "Well done! #{@guess.upcase} is in the grid and it is a valid english word!"
     end
   end
